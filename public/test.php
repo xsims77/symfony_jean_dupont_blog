9d93aa31b28154701ea60774a6db3862ff427be1
xsims77 <simsxiong@gmail.com>
2023-08-23 12:27:59 +0200

    Mis en place des articles, catégories et tags aux visiteurs

---
+++ public/test.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
--- src/Controller/Visitor/Blog/BlogController.php
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -2,20 +2,37 @@
 
 namespace App\Controller\Visitor\Blog;
 
-use App\Repository\CategoryRepository;
+use App\Entity\Post;
 use App\Repository\TagRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     #[Route('/blog/all-posts', name: 'visitor.blog.index')]
-    public function index(CategoryRepository $categoryRepository, TagRepository $tagRepository): Response
+    public function index(CategoryRepository $categoryRepository, TagRepository $tagRepository, PostRepository $postRepository): Response
     {
         $categories = $categoryRepository->findAll();
 
         $tags = $tagRepository->findAll();
-        return $this->render('pages/visitor/blog/index.html.twig', compact('categories', 'tags'));
+
+        $posts = $postRepository->findBy(['isPublished'=> true], ['publishedAt' => 'DESC']);
+
+        return $this->render('pages/visitor/blog/index.html.twig', [
+            'categories' => $categories,
+            'tags'       => $tags,
+            'posts'      => $posts,
+        ]);
+    }
+
+    #[Route('/blog/post/{id}/{slug}', name: 'visitor.blog.post.show', methods:['GET'])]
+    public function show(Post $post) : Response
+    {
+        return $this->render("pages/visitor/blog/show.html.twig", [
+            "post" => $post
+        ]);
     }
 }
